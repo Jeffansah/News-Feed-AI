4f4c1e4f51132422f72169141bdccaa526f6ff87
Jeffrey Owusu Ansah <owusuansaj@coventry.ac.uk>
2022-11-17 10:50:21 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NewsCards from "./components/NewsCards/NewsCards";
+
+const alanKey = "a563a308af0faee3a2c099ada24a1c952e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key : alanKey,
+            onCommand: ({command, articles}) => {
+                if(command === "newHeadlines"){
+                    setNewsArticles(articles);
+                }
+            }
+
+        })
+    }, [])
+
+    return (
+        <div>
+            <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles} />
+        </div>
+    );
 }
 
 export default App;
